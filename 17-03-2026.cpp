ffbdb273df1951d9746a60f58ba04c8bccd3fb8b
elizazha <lizokbella@mail.ru>
2026-03-17 11:13:14 +0300

    tests

--- 17-03-2026.cpp
+++ 17-03-2026.cpp
@@ -1,5 +1,28 @@
 #include <iostream>
+#include "top_it_vector.hpp"
 
+bool testEmptyVector()
+{
+  return false;
+}
 
 int main()
-{}
+{
+  using test_t = std::pair< const char*, bool(*)() >;
+  test_t tests[] = {
+    {"Empty vector", testEmptyVector}
+  };
+  // bool(*tests[])() = {//массик указателей ничего не принимающий восвращающий логическое
+  //   testEmptyVector
+  // };
+  const size_t count = sizeof(tests) / sizeof(test_t);
+  std::cout << std::boolalpha;
+  bool pass = true;
+  for(size_t i = 0; i <count; i++)
+  {
+    bool res = tests[i].second();
+    std::cout << tests[i].first << ": " << res << "\n";
+    pass = pass && res;
+  }
+  std::cout << "Result: " << pass << "\n";
+}
--- top_it_vector.hpp
+++ top_it_vector.hpp
@@ -1,10 +1,29 @@
 #ifndef TOP-IT-VECTOR_HPP
 #define TOP-IT-VECTOR_HPP
+#include <cstddef>
+namespace topit
+{
+  template< class T >
+  struct Vector{
+    ~Vector();
+    Vector();
+
+    private:
+      T* data_;
+      size_t size_, capacity_;
+  };
+}
+
+template < class T >
+topit::Vector< T >::~Vector()
+{}
 
 template< class T >
-struct Vector{
-  T* data;
-  size_t size, capacity;
-};
+topit::Vector< T > :: Vector():
+  data_(),
+  size_(),
+  capacity_()
+{}
+
 
 #endif
