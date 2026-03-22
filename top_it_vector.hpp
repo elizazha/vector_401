ba86e938f5b9ed2c407b692f6680828dd3d6d336
elizazha <lizokbella@mail.ru>
2026-03-23 01:14:03 +0300

    added isEmpty(), getSize(), getCapacity(), pushback(...), popback();

--- top_it_vector.hpp
+++ top_it_vector.hpp
@@ -1,5 +1,5 @@
-#ifndef TOP-IT-VECTOR_HPP
-#define TOP-IT-VECTOR_HPP
+#ifndef TOP_IT_VECTOR_HPP
+#define TOP_IT_VECTOR_HPP
 #include <cstddef>
 namespace topit
 {
@@ -7,15 +7,14 @@ namespace topit
   struct Vector{
     ~Vector();
     Vector();
-    Vector(const Vecttor&);
+    Vector(const Vector&);
     Vector(Vector&&);
     Vector& operator=(const Vector&);
     Vector& operator=(Vector&&);
 
-
-    bool isEmpty() const noexcept;//дз реализоввать + тесты
-    size_t getSize() const noexcept;//дз реализоввать + тесты
-    size_t getCapacity() const noexcept;//дз реализоввать + тесты
+    bool isEmpty() const noexcept;//дз реализоввать + тесты 0
+    size_t getSize() const noexcept;//дз реализоввать + тесты 0
+    size_t getCapacity() const noexcept;//дз реализоввать + тесты 0
 
     void pushback(const T& v);//дз реализоввать + тесты
     void popback();//дз реализоввать + тесты
@@ -49,6 +48,56 @@ template< class T >
 bool topit::Vector<T>::isEmpty() const noexcept
 {
   return !size_;
+}
+
+template< class T >
+size_t topit::Vector<T>::getSize() const noexcept
+{
+  return size_;
+}
+
+template< class T >
+size_t topit::Vector<T>::getCapacity() const noexcept
+{
+  return capacity_;
+}
+
+template< class T >
+void topit::Vector<T>::pushback(const T& v)
+{
+  if(size_ == capacity_)
+  {
+    size_t newCap;
+    if(capacity_ == 0)
+    {
+      newCap = 1;
+    }else
+    {
+      newCap = capacity_ + 1;
+    }
+    T* newData = new T[newCap];
+    for(size_t i = 0; i < size_; i++)
+    {
+      newData[i] = data_[i];
+    }
+
+    delete[] data_;
+    data_ = newData;
+    capacity_ = newCap;
+  }
+
+  data_[size_] = v;
+  size_++;
+}
+
+template<class T>
+void topit::Vector<T>::popback()
+{
+  if(size_ == 0)
+  {
+    throw std::out_of_range("size is zero; no data")
+  }
+  size_--;
 }
 
 #endif
